8555d280816f760ed9afd546fd65b68ae7180819
Jeff Lutgen <jlutgen@gmail.com>
2021-08-31 21:44:24 -0700

    init

---
+++ nano/blinky/main.c
@@ -0,0 +1,21 @@
+#ifndef F_CPU
+#define F_CPU 16000000UL
+#endif
+ 
+#include <avr/io.h>
+#include <util/delay.h>
+ 
+int main(void)
+{
+    DDRB = 0b00100000; // PB5: output pin
+    while(1)
+    {
+        // LED on
+        PORTB = 0b00100000;
+        _delay_ms(500);
+ 
+        //LED off
+        PORTB = 0b00000000;
+        _delay_ms(500);
+    }
+}
---
+++ nano/makefile-variables.mk
@@ -0,0 +1,43 @@
+############################################################
+#
+# Makefile for Whittier College AVR projects
+#
+# Jeff Lutgen
+#
+#############################################################
+
+# Processor type
+PROCESSOR=atmega328p
+
+ifdef WSL_DISTRO_NAME        # Windows Subsystem for Linux
+	PORT=todo
+else
+	PORT=/dev/cu.usbserial-1410
+endif
+
+CROSS=avr
+
+# The C compiler
+CC=$(CROSS)-gcc
+
+# The bin creator
+HX=$(CROSS)-objcopy
+
+# The object dumper
+OBJDMP=$(CROSS)-objdump
+
+# The size-reporting tool
+SIZE=$(CROSS)-size
+
+# The utility for writing a .bin file to the MCU.
+WRITE=avrdude
+
+# The output target $(TARGET).hex
+TARGET=out
+
+CFLAGS = \
+	-ggdb3 -Os -Wall \
+	-std=gnu11 \
+	-mmcu=$(PROCESSOR)
+
+LINKFLAGS=-Wl,-Map=$(TARGET).map
